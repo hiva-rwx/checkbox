f9edeb2ad416583d1985c2adf230552f158c49ff
mahdi <mahdi.hdd2001@gmail.com>
2022-01-17 14:44:34 +0330

    stable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Children, cloneElement, useEffect, useState } from "react";
 
-function App() {
+const Checkbox = ({ children }) => {
+  const [checked, setChecked] = useState(true);
+
+  const allChildren = Children.map(children, (child) => {
+    if (child.type !== Label && child.type !== InputCheckbox) {
+      return null;
+    }
+    const clone = cloneElement(child, {
+      checked,
+      setChecked,
+    });
+    return clone;
+  });
+  return allChildren;
+};
+
+const InputCheckbox = ({ checked, setChecked }) => {
+  const [_checked, _setChecked] = useState(!!checked);
+  useEffect(() => {
+    if (!setChecked) {
+      console.warn("single element");
+    }
+  }, [_checked, checked, setChecked]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <input
+      type="checkbox"
+      checked={!setChecked ? _checked : checked}
+      onChange={(e) => {
+        if (setChecked) {
+          setChecked(e.target.checked);
+        }
+        _setChecked(e.target.checked);
+      }}
+    />
   );
-}
+};
+const Label = ({ setChecked, children }) => {
+  if (!setChecked) {
+    throw new Error("single element");
+  }
+  return (
+    <label
+      style={{ userSelect: "none" }}
+      onClick={() => {
+        setChecked((state) => !state);
+      }}
+    >
+      {children}
+    </label>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <Checkbox>
+        <Label>{"label for checkbox"}</Label>
+        <InputCheckbox />
+      </Checkbox>
+      {/* single checkbox */}
+      <br />
+      <span>single checkbox {'(not label)'}</span>
+      <InputCheckbox />
+    </>
+  );
+};
 
 export default App;
